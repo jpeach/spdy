30c203703f0424fe1c68eabfe730b7df34c918af
James Peach <jamespeach@me.com>
2012-02-26 16:44:57 -0800

    Add missing license headers

--- src/lib/spdy/spdy.h
+++ src/lib/spdy/spdy.h
@@ -1,6 +1,17 @@
 /*
- * Copyright (c) 2011 James Peach <jamespeach@me.com>
+ * Copyright (c) 2012 James Peach <jamespeach@me.com>
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #ifndef SPDY_H_57211D6A_F320_42E3_8205_89E651B4A5DB
